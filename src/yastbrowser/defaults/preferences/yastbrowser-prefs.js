50cc192bfc734eff2cf9d31c3b993c1fc98c2083
J. Daniel Schmidt <jdsn@suse.de>
2006-03-14 17:05:36 +0000

    new yastbrowser, correct title, self.close() now works
    svn path=/trunk/installation/; revision=28974

--- src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
+++ src/yastbrowser/defaults/preferences/yastbrowser-prefs.js
@@ -1,2 +1,4 @@
 pref("toolkit.defaultChromeURI", "chrome://yastbrowser/content/yastbrowser.xul");
 pref("general.useragent.extra.yastbrowser", "YaST-Browser/0.1");
+pref("security.warn_entering_secure", false);
+pref("dom.allow_scripts_to_close_windows", true);
