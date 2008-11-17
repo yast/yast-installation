09de592f90e22a93c81dd4c29def5fea1fd84076
Arvin Schnell <aschnell@suse.de>
2008-11-17 10:49:46 +0000

    - more ids for "special" hardhare
    svn path=/trunk/installation/; revision=53357

--- startup/arch/i386/x11.sh
+++ startup/arch/i386/x11.sh
@@ -2,8 +2,8 @@
 
 function i386_check_x11 () {
 	if lspci -n | grep "0300: 8086" | \
-           grep -q -E "7121|7123|7125|1132"; then
-		log "\tHardware dependent: use accelerated driver on Intel 810/815"
+           grep -q -E "7121|7123|7125|1132|27ae"; then
+		log "\tHardware dependent: use accelerated driver on Intel 810/815/945GME"
 		export acceleratedx=1
 	fi
 }
