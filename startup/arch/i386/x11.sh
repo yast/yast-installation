65c11fca2090996aeb0108ca61e34ed633cd4b71
Lukáš Ocilka <locilka@suse.cz>
2008-11-19 15:04:59 +0000

    - Added pciutils to Requires, lspci was called but not required   (bnc #446533). - 2.17.37
    svn path=/trunk/installation/; revision=53430

--- startup/arch/i386/x11.sh
+++ startup/arch/i386/x11.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 function i386_check_x11 () {
-	if lspci -n | grep "0300: 8086" | \
+	if test -x /sbin/lspci && /sbin/lspci -n | grep "0300: 8086" | \
            grep -q -E "7121|7123|7125|1132|27ae"; then
 		log "\tHardware dependent: use accelerated driver on Intel 810/815/945GME"
 		export acceleratedx=1
