3eb62871e4b7349cd610c1cccf323576812c0adc
Arvin Schnell <aschnell@suse.de>
2009-06-08 12:19:52 +0000

    - use minimalistic xorg.conf during installation (bnc #510015)
    svn path=/trunk/installation/; revision=57486

--- startup/arch/i386/x11.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-function i386_check_x11 () {
-	if test -x /sbin/lspci && /sbin/lspci -n | grep "0300: 8086" | \
-           grep -q -E "7121|7123|7125|1132|27ae"; then
-		log "\tHardware dependent: use accelerated driver on Intel 810/815/945GME"
-		export acceleratedx=1
-	fi
-}
