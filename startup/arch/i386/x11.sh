4f8dbe7dcbb61a7297706c7d5056be18710012fd
Arvin Schnell <aschnell@suse.de>
2011-10-04 09:25:30 +0000

    - don't call lspci on s390 (bnc#720108)
    svn path=/branches/SuSE-Code-11-SP2-Branch/installation/; revision=66217

--- startup/arch/i386/x11.sh
+++ startup/arch/i386/x11.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
 function i386_check_x11 () {
+	[ -n "$HOSTTYPE" ] || HOSTTYPE=$(arch)
+	if [ "$HOSTTYPE" == "s390" -o "$HOSTTYPE" == "s390x" ] ; then
+		return
+	fi
+    
 	if test -x /sbin/lspci && /sbin/lspci -n | grep -q "0300: 8086"; then
 		log "\tHardware dependent: use accelerated driver on Intel hardware"
 		export acceleratedx=1
