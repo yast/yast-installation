0cff7ddb6076ef1a0e056ddfacfbdd48e3fc82ad
Lukáš Ocilka <locilka@suse.cz>
2012-01-20 14:55:33 +0000

    - Not using native driver on Intel graphics hardware during   installation anymore (BNC #740517). - 2.17.99
    svn path=/branches/SuSE-Code-11-SP2-Branch/installation/; revision=67241

--- startup/arch/i386/x11.sh
+++ startup/arch/i386/x11.sh
@@ -5,9 +5,4 @@ function i386_check_x11 () {
 	if [ "$HOSTTYPE" == "s390" -o "$HOSTTYPE" == "s390x" ] ; then
 		return
 	fi
-    
-	if test -x /sbin/lspci && /sbin/lspci -n | grep -q "0300: 8086"; then
-		log "\tHardware dependent: use accelerated driver on Intel hardware"
-		export acceleratedx=1
-	fi
 }
