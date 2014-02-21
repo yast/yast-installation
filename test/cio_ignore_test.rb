2c2a53da96f2fd37636fcae067e5caf429a1169d
Josef Reidinger <jreidinger@suse.cz>
2014-02-21 17:42:37 +0100

    improve output of tests

--- test/cio_ignore_test.rb
+++ test/cio_ignore_test.rb
@@ -15,46 +15,52 @@ describe ::Installation::CIOIgnoreProposal do
   end
 
   describe "#run" do
-    it "returns proposal entry hash when \"MakeProposal\" passed" do
-      result = subject.run("MakeProposal")
+    describe "first parameter \"MakeProposal\"" do
+      it "returns proposal entry hash containing \"links\", \"help\" and \"preformatted_proposal\"" do
+        result = subject.run("MakeProposal")
 
-      expect(result).to have_key("links")
-      expect(result).to have_key("help")
-      expect(result).to have_key("preformatted_proposal")
+        expect(result).to have_key("links")
+        expect(result).to have_key("help")
+        expect(result).to have_key("preformatted_proposal")
+      end
     end
 
-    it "returns proposal metadata hash when \"Description\" passed" do
-      result = subject.run("Description")
+    describe "first parameter \"Description\"" do
+      it "returns proposal metadata hash containing \"rich_text_title\", \"id\" and \"menu_title\"" do
+        result = subject.run("Description")
 
-      expect(result).to have_key("rich_text_title")
-      expect(result).to have_key("menu_title")
-      expect(result).to have_key("id")
+        expect(result).to have_key("rich_text_title")
+        expect(result).to have_key("menu_title")
+        expect(result).to have_key("id")
+      end
     end
 
-    it "changes proposal if \"AskUser\" passed with chosen_id in second param hash" do
-      params = [
-        "AskUser",
-        "chosen_id" => ::Installation::CIOIgnoreProposal::CIO_DISABLE_LINK
-      ]
-      result = subject.run(*params)
+    describe "first parameter \"AskUser\"" do
+      it "changes proposal if passed with chosen_id in second param hash" do
+        params = [
+          "AskUser",
+          "chosen_id" => ::Installation::CIOIgnoreProposal::CIO_DISABLE_LINK
+        ]
+        result = subject.run(*params)
 
-      expect(result["workflow_sequence"]).to eq :next
-      expect(::Installation::CIOIgnore.instance.enabled).to be false
-    end
+        expect(result["workflow_sequence"]).to eq :next
+        expect(::Installation::CIOIgnore.instance.enabled).to be false
+      end
 
-    it "raises RuntimeError if \"AskUser\" passed without chosen_id in second param hash" do
-      expect{subject.run("AskUser")}.to(
-        raise_error(RuntimeError)
-      )
-    end
+      it "raises RuntimeError if passed without chosen_id in second param hash" do
+        expect{subject.run("AskUser")}.to(
+          raise_error(RuntimeError)
+        )
+      end
 
-    it "raises RuntimeError if \"AskUser\" passed with non-existing chosen_id in second param hash" do
-      params = [
-        "AskUser",
-        "chosen_id" => "non_existing"
-      ]
+      it "raises RuntimeError if \"AskUser\" passed with non-existing chosen_id in second param hash" do
+        params = [
+          "AskUser",
+          "chosen_id" => "non_existing"
+        ]
 
-      expect{subject.run(*params)}.to raise_error(RuntimeError)
+        expect{subject.run(*params)}.to raise_error(RuntimeError)
+      end
     end
 
     it "raises RuntimeError if unknown action passed as first parameter" do
@@ -90,7 +96,7 @@ describe ::Installation::CIOIgnoreFinish do
 
     end
 
-    describe "first paramater \"Write\"" do
+    describe "first parameter \"Write\"" do
       describe "Device blacklisting is disabled" do
         it "do nothing" do
           ::Installation::CIOIgnore.instance.enabled = false
