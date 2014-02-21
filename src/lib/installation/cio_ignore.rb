32b8d92f97f3993ba1ddc123828ef1491f621d7f
Josef Reidinger <jreidinger@suse.cz>
2014-02-21 15:39:57 +0100

    initial cio ignore support - finish proposal class

--- src/lib/installation/cio_ignore.rb
+++ src/lib/installation/cio_ignore.rb
@@ -16,6 +16,7 @@ module Installation
   end
 
   class CIOIgnoreProposal
+    include Yast::Logger
     include Yast::I18n
 
     CIO_ENABLE_LINK = "cio_enable"
@@ -30,11 +31,23 @@ module Installation
       func = args.first
       param = args[1] || {}
 
+      log.debug "cio ignore client called with #{func} and #{param}"
+
       case func
       when "MakeProposal"
         proposal_entry
+      when "Description"
+        {
+          # this is a heading
+          "rich_text_title" => _("Blacklist Devices"),
+          # this is a menu entry
+          "menu_title"      => _("B&lacklist Devices"),
+          "id"              => CIO_ACTION_ID
+        }
+      when "AskUser"
+        edit param["chosen_id"]
       else
-        raise "uknown method"
+        raise "Uknown action passed as first parameter"
       end
     end
 
@@ -68,6 +81,24 @@ module Installation
           "<p>Use <b>Blacklist devices</b> if you want to create blacklist channels to such devices which will reduce kernel memory footprint.</p>"
         )
       }
+    end
+
+    def edit edit_id
+        raise "Internal error: no id passed to proposal edit" unless edit_id
+
+        log.info "CIO proposal change requested, id #{edit_id}"
+
+        cio_ignore = CIOIgnore.instance
+
+        cio_ignore.enabled = case edit_id
+          when CIO_DISABLE_LINK then false
+          when CIO_ENABLE_LINK  then true
+          when CIO_ACTION_ID    then !cio_ignore.enabled
+          else
+            raise "Unexpected value #{edit_id}"
+          end
+
+        { "workflow_sequence" => :next }
     end
   end
 end
--- test/cio_ignore_test.rb
+++ test/cio_ignore_test.rb
@@ -7,14 +7,61 @@ require "yast"
 require "installation/cio_ignore"
 
 describe ::Installation::CIOIgnoreProposal do
-  describe "#run" do
-   it "returns proposal entry hash when \"MakeProposal\" passed" do
-     result = ::Installation::CIOIgnoreProposal.new.run(["MakeProposal"])
 
-     expect(result).to have_key("links")
-     expect(result).to have_key("help")
-     expect(result).to have_key("preformatted_proposal")
-   end
+  subject { ::Installation::CIOIgnoreProposal.new }
+
+  before(:each) do
+    ::Installation::CIOIgnore.instance.reset
+  end
+
+  describe "#run" do
+    it "returns proposal entry hash when \"MakeProposal\" passed" do
+      result = subject.run(["MakeProposal"])
+
+      expect(result).to have_key("links")
+      expect(result).to have_key("help")
+      expect(result).to have_key("preformatted_proposal")
+    end
+
+    it "returns proposal metadata hash when \"Description\" passed" do
+      result = subject.run(["Description"])
+
+      expect(result).to have_key("rich_text_title")
+      expect(result).to have_key("menu_title")
+      expect(result).to have_key("id")
+    end
+
+    it "changes proposal if \"AskUser\" passed with chosen_id in second param hash" do
+      params = [
+        "AskUser",
+        "chosen_id" => ::Installation::CIOIgnoreProposal::CIO_DISABLE_LINK
+      ]
+      result = subject.run(params)
+
+      expect(result["workflow_sequence"]).to eq :next
+      expect(::Installation::CIOIgnore.instance.enabled).to be false
+    end
+
+    it "raises RuntimeError if \"AskUser\" passed without chosen_id in second param hash" do
+      expect{subject.run(["AskUser"])}.to(
+        raise_error(RuntimeError)
+      )
+    end
+
+    it "raises RuntimeError if \"AskUser\" passed with non-existing chosen_id in second param hash" do
+      params = [
+        "AskUser",
+        "chosen_id" => "non_existing"
+      ]
+
+      expect{subject.run(params)}.to raise_error(RuntimeError)
+    end
+
+    it "raises RuntimeError if unknown action passed as first parameter" do
+      expect{subject.run(["non_existing_action"])}.to(
+        raise_error(RuntimeError)
+      )
+    end
   end
 end
 
