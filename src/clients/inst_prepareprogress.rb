7f25faeda9945cea4b5df75cef8c641e3f417acd
Josef Reidinger <jreidinger@suse.cz>
2014-04-14 16:38:08 +0200

    return error message if base product not found and continue in installation(bnc#873458)

--- src/clients/inst_prepareprogress.rb
+++ src/clients/inst_prepareprogress.rb
@@ -41,6 +41,7 @@ module Yast
       Yast.import "PackageSlideShow"
       Yast.import "Wizard"
       Yast.import "InstData"
+      Yast.import "Popup"
       Yast.import "Product"
 
       Builtins.y2milestone("BEGIN of inst_prepareprogress.ycp")
@@ -48,7 +49,13 @@ module Yast
       # hide the RN button and set the release notes for SlideShow (bnc#871158)
       Wizard.HideReleaseNotesButton
       base_products = Product.FindBaseProducts
-      SlideShow.SetReleaseNotes(InstData.release_notes, base_products[0]["name"])
+      if !base_products || base_products.empty?
+        Builtins.y2error "base product not found"
+        Popup.error "Cannot find base product. Release notes won't be shown."
+        SlideShow.SetReleaseNotes(InstData.release_notes, "")
+      else
+        SlideShow.SetReleaseNotes(InstData.release_notes, base_products[0]["name"])
+      end
 
       Packages.SlideShowSetUp(Language.language)
 
