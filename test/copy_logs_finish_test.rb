48183d09494e3c921c5d1699c9263cb4856f736c
Josef Reidinger <jreidinger@suse.cz>
2015-01-07 16:43:57 +0100

    DRY tests

--- test/copy_logs_finish_test.rb
+++ test/copy_logs_finish_test.rb
@@ -10,42 +10,50 @@ describe ::Installation::CopyLogsFinish do
       allow(Yast::WFM).to receive(:Execute)
     end
 
+    def mock_log_dir(files)
+      allow(Yast::WFM).to receive(:Read).and_return(files)
+    end
+
+    def expect_to_run(cmd)
+      expect(Yast::WFM).to receive(:Execute).with(anything(), cmd)
+    end
+
     it "copies logs from instalation to target system" do
-      allow(Yast::WFM).to receive(:Read).and_return(["y2start.log"])
+      mock_log_dir(["y2start.log"])
 
-      expect(Yast::WFM).to receive(:Execute).with(anything(), /cp/).at_least(:once)
-
-      subject.write
-    end
-
-    it "rotate y2log" do
-      allow(Yast::WFM).to receive(:Read).and_return(["y2log-1.gz"])
-
-      expect(Yast::WFM).to receive(:Execute).with(anything(), /cp .*y2log-1.gz .*y2log-2.gz/)
+      expect_to_run(/cp .*y2start.log .*y2start.log/)
 
       subject.write
     end
 
-    it "compress y2log if not already done" do
-      allow(Yast::WFM).to receive(:Read).and_return(["y2log-1"])
+    it "rotates y2log" do
+      mock_log_dir(["y2log-1.gz"])
 
-      expect(Yast::WFM).to receive(:Execute).with(anything(), /gzip .*y2log-2/) #-2 due to rotation
-
-      subject.write
-    end
-
-    it "do not stuck during compress if file already exists (bnc#897091)" do
-      allow(Yast::WFM).to receive(:Read).and_return(["y2log-1"])
-
-      expect(Yast::WFM).to receive(:Execute).with(anything(), /gzip -f/)
+      expect_to_run(/cp .*y2log-1.gz .*y2log-2.gz/)
 
       subject.write
     end
 
-    it "rotate zypp.log" do
-      allow(Yast::WFM).to receive(:Read).and_return(["zypp.log"])
+    it "compresses y2log if not already done" do
+      mock_log_dir(["y2log-1"])
 
-      expect(Yast::WFM).to receive(:Execute).with(anything(), /cp .*zypp.log .*zypp.log-1/)
+      expect_to_run(/gzip .*y2log-2/) #-2 due to rotation
+
+      subject.write
+    end
+
+    it "does not stuck during compress if file already exists (bnc#897091)" do
+      mock_log_dir(["y2log-1"])
+
+      expect_to_run(/gzip -f/)
+
+      subject.write
+    end
+
+    it "rotates zypp.log" do
+      mock_log_dir(["zypp.log"])
+
+      expect_to_run(/cp .*zypp.log .*zypp.log-1/)
 
       subject.write
     end
