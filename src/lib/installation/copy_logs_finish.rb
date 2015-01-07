7c098f8b4958e2941c59261f06c155e0289acfa0
Josef Reidinger <jreidinger@suse.cz>
2015-01-07 14:08:59 +0100

    rubify code

--- src/lib/installation/copy_logs_finish.rb
+++ src/lib/installation/copy_logs_finish.rb
@@ -17,6 +17,8 @@
 # current contact information at www.novell.com.
 # ------------------------------------------------------------------------------
 
+require "yast"
+
 require "installation/finish_client"
 
 module Installation
@@ -24,8 +26,6 @@ module Installation
     include Yast::I18n
 
     def initialize
-      Yast.import "UI"
-
       textdomain "installation"
 
       Yast.import "Directory"
@@ -45,63 +45,49 @@ module Installation
     end
 
     def write
-      @log_files = Yast::Convert.convert(
-        Yast::WFM.Read(path(".local.dir"), Yast::Directory.logdir),
-        :from => "any",
-        :to   => "list <string>"
-      )
+      log_files = Yast::WFM.Read(Yast::Path.new(".local.dir"), Yast::Directory.logdir)
 
-      Yast::Builtins.foreach(@log_files) do |file|
+      log_files.each do |file|
         log.debug "Processing file #{file}"
 
-        if file == "y2log" || Yast::Builtins.regexpmatch(file, "^y2log-[0-9]+$")
+        if file == "y2log" || file =~ /\Ay2log-\d+\z/
           # Prepare y2log, y2log-* for log rotation
-
           target_no = 1
 
-          if Yast::Ops.greater_than(Yast::Builtins.size(file), Yast::Builtins.size("y2log-"))
-            target_no = Yast::Ops.add(
-              1,
-              Yast::Builtins.tointeger(
-                Yast::Builtins.substring(file, Builtins.size("y2log-"), 5)
-              )
-            )
+          if file != "y2log"
+            prefix_size = "y2log-".size
+            target_no = file[prefix_size..-1].to_i + 1
           end
 
-          target_basename = Yast::Builtins.sformat("y2log-%1", target_no)
+          target_basename = "y2log-#{target_no}"
           InjectRenamedFile(Yast::Directory.logdir, file, target_basename)
 
-          compress_cmd = Yast::Builtins.sformat(
-            "gzip %1/%2/%3",
+          target_path = ::File.join(
             Yast::Installation.destdir,
             Yast::Directory.logdir,
             target_basename
           )
+          compress_cmd = "gzip #{target_path}"
           log.debug "Compress command: #{compress_cmd}"
-          Yast::WFM.Execute(path(".local.bash"), compress_cmd)
-        elsif Yast::Builtins.regexpmatch(file, "^y2log-[0-9]+\\.gz$")
-          target_no = Yast::Ops.add(
-            1,
-            Yast::Builtins.tointeger(
-              Yast::Builtins.regexpsub(file, "y2log-([0-9]+)\\.gz", "\\1")
-            )
-          )
+          Yast::WFM.Execute(Yast::Path.new(".local.bash"), compress_cmd)
+        elsif file =~ /\Ay2log-\d+\.gz\z/
+          target_no = file[/y2log-(\d+)/, 1].to_i + 1
           InjectRenamedFile(
             Yast::Directory.logdir,
             file,
-            Yast::Builtins.sformat("y2log-%1.gz", target_no)
+            "y2log-#{target_no}"
           )
         elsif file == "zypp.log"
           # Save zypp.log from the inst-sys
           InjectRenamedFile(Yast::Directory.logdir, file, "zypp.log-1") # not y2log, y2log-*
         else
-          InjectFile(Yast::Ops.add(Yast::Ops.add(Yast::Directory.logdir, "/"), file))
+          InjectFile(::File.join(Yast::Directory.logdir, file))
         end
       end
 
       copy_cmd = "/bin/cp /var/log/pbl.log '#{Yast::Installation.destdir}/#{Yast::Directory.logdir}/pbl-instsys.log'"
       log.debug "Copy command: #{copy_cmd}"
-      Yast::WFM.Execute(path(".local.bash"), copy_cmd)
+      Yast::WFM.Execute(Yast::Path.new(".local.bash"), copy_cmd)
 
       nil
     end
