5e0355c7b7eaf3e483990eb55ff6cd0f413b47ce
Martin Vidner <mvidner@suse.cz>
2015-07-17 11:14:44 +0200

    Enabled coverage reporting to Coveralls
    https://coveralls.io/github/yast/yast-installation

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,27 @@ srcdir = File.expand_path("../../src", __FILE__)
 y2dirs = ENV.fetch("Y2DIR", "").split(":")
 ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
 
+require "yast"
+
 if ENV["COVERAGE"]
   require "simplecov"
-  SimpleCov.start
-end
+  SimpleCov.start do
+    add_filter "/test/"
+  end
 
-require "yast"
+  # For coverage we need to load all ruby files
+  # Note that clients/ are excluded because they run too eagerly by
+  # design
+  Dir["#{srcdir}/{include,lib,modules}/**/*.rb"].each do |f|
+    require_relative f
+  end
+
+  # use coveralls for on-line code coverage reporting at Travis CI
+  if ENV["TRAVIS"]
+    require "coveralls"
+    SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+      SimpleCov::Formatter::HTMLFormatter,
+      Coveralls::SimpleCov::Formatter
+    ]
+  end
+end
