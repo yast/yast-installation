0acdb0ed19ee4e5eaed00872004c217bbbb3fef7
Martin Vidner <mvidner@suse.cz>
2015-07-17 11:00:33 +0200

    Respect existing Y2DIR for tests.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,6 @@
-ENV["Y2DIR"] = File.expand_path("../../src", __FILE__)
+srcdir = File.expand_path("../../src", __FILE__)
+y2dirs = ENV.fetch("Y2DIR", "").split(":")
+ENV["Y2DIR"] = y2dirs.unshift(srcdir).join(":")
 
 if ENV["COVERAGE"]
   require "simplecov"
