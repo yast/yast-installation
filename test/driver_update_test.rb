0c5716ffe626d1e4ccae11c29ba82370cb478481
Imobach González Sosa <igonzalezsosa@suse.com>
2016-03-03 15:27:48 +0000

    Execute update scripts when applying Driver Update

--- test/driver_update_test.rb
+++ test/driver_update_test.rb
@@ -13,11 +13,21 @@ Yast.import "Linuxrc"
 
 describe Installation::DriverUpdate do
   TEST_DIR = Pathname.new(__FILE__).dirname
+  TEMP_DIR = TEST_DIR.join("test", "tmp", "update")
   FIXTURES_DIR = TEST_DIR.join("fixtures")
 
   let(:url) { URI("file://#{FIXTURES_DIR}/fake.dud") }
 
   subject { Installation::DriverUpdate.new(url) }
+
+  before do
+    allow(Yast::Linuxrc).to receive(:InstallInf).with("UpdateDir")
+      .and_return("/linux/suse/x86_64-sles12")
+  end
+
+  after do
+    FileUtils.rm_r(TEMP_DIR) if TEMP_DIR.exist?
+  end
 
   describe "#fetch" do
     let(:target) { TEST_DIR.join("target") }
@@ -29,8 +39,6 @@ describe Installation::DriverUpdate do
     let(:dud_io) { StringIO.new(File.binread(FIXTURES_DIR.join("fake.dud"))) }
 
     it "downloads the file at #url and stores in the given directory" do
-      allow(Yast::Linuxrc).to receive(:InstallInf).with("UpdateDir")
-        .and_return("/linux/suse/x86_64-sles12")
       subject.fetch(target)
       expect(target.join("dud.config")).to be_file
     end
@@ -42,25 +50,22 @@ describe Installation::DriverUpdate do
         expect { subject.fetch(target) }.to raise_error StandardError
       end
     end
-
-    context "when the destination directory does not exists" do
-      let(:target) { Pathname.pwd.join("non-existent-directory") }
-
-      it "raises an exception" do
-        expect { subject.fetch(target) }.to raise_error StandardError
-      end
-    end
   end
 
   describe "#apply!" do
-    let(:local_path) { Pathname.new("/updates/001") }
+    let(:local_path) { TEMP_DIR.join("000") }
 
-    it "applies the DUD to the running system" do
-      allow(subject).to receive(:local_path).and_return(local_path)
-      expect(Yast::SCR).to receive(:Execute)
-        .with(Yast::Path.new(".target.bash_output"), "/etc/adddir #{local_path}/inst-sys /")
-        .and_return("exit" => 0)
-      subject.apply!
+    context "when the remote file was fetched" do
+      before do
+        subject.fetch(local_path)
+      end
+
+      it "applies the DUD to the running system" do
+        expect(Yast::SCR).to receive(:Execute)
+          .with(Yast::Path.new(".target.bash_output"), "/etc/adddir #{local_path}/inst-sys /")
+          .and_return("exit" => 0)
+        subject.apply!
+      end
     end
 
     context "when the remote file was not fetched" do
