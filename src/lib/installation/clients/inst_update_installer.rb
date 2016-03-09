cf0db1a14035f0f3b891964e4ecf65167392b993
Imobach González Sosa <igonzalezsosa@suse.com>
2016-03-09 10:35:54 +0000

    Read URL for self-update from Linuxrc and control file

--- src/lib/installation/clients/inst_update_installer.rb
+++ src/lib/installation/clients/inst_update_installer.rb
@@ -20,22 +20,22 @@ module Yast
   class InstUpdateInstaller < Client
     include Yast::Logger
 
-    UPDATED_FILENAME = "installer_updated"
+    UPDATED_FLAG_FILENAME = "installer_updated"
+    URL_SUPPORTED_SCHEMES = ["http", "https", "ftp"]
 
     # TODO
     #
-    # * Show progress
-    # * Check if network is enabled
     # * Handle unsigned files
-    # * Get URL from control file or Linuxrc
     def main
       Yast.import "Directory"
       Yast.import "Installation"
+      Yast.import "ProductFeatures"
+      Yast.import "Linuxrc"
 
-      return :next if installer_updated?
+      return :next if installer_updated? || !self_update_enabled?
 
       if update_installer
-        ::FileUtils.touch(update_file) # Indicates that the installer was updated.
+        ::FileUtils.touch(update_flag_file) # Indicates that the installer was updated.
         ::FileUtils.touch(Installation.restart_file)
         :restart_yast # restart YaST to apply modifications.
       else
@@ -48,29 +48,78 @@ module Yast
     # @return [Boolean] true if installer was updated; false otherwise.
     def update_installer
       manager = ::Installation::UpdatesManager.new
-      if manager.add_update(URI("http://192.168.122.1:3000/fake.dud"))
+      if manager.add_update(self_update_url)
         manager.apply_all
       else
         false
       end
     end
 
+    # Determines whether self-update feature is enabled
+    #
+    # * Check whether is disabled via Linuxrc
+    # * Otherwise, it's considered as enabled if some URL is defined.
+    #
+    # @return [Boolean] True if it's enabled; false otherwise.
+    def self_update_enabled?
+      if Linuxrc.InstallInf("SelfUpdate") == "0" # disabled via Linuxrc
+        false
+      else
+        !self_update_url.nil?
+      end
+    end
+
+    # Return the self-update URL
+    #
+    # @return [URI] self-update URL
+    #
+    # @see #self_update_url_from_linuxrc
+    # @see #self_update_url_from_control
+    def self_update_url
+      self_update_url_from_linuxrc || self_update_url_from_control
+    end
+
+    # Return the self-update URL according to Linuxrc
+    #
+    # @return [URI,nil] self-update URL. nil if no URL was set in Linuxrc.
+    def self_update_url_from_linuxrc
+      url = URI(Linuxrc.InstallInf("SelfUpdate") || "")
+      valid_url?(url) ? url : nil
+    end
+
+    # Return the self-update URL according to product's control file
+    #
+    # @return [URI,nil] self-update URL. nil if no URL was set in control file.
+    def self_update_url_from_control
+      url = URI(ProductFeatures.GetStringFeature("globals", "self_update_url"))
+      valid_url?(url) ? url : nil
+    end
+
+    # Determines whether the URL is valid or no
+    #
+    # @return [Boolean] True if it's valid; false otherwise.
+    #
+    # @see URL_SUPPORTED_SCHEMES
+    def valid_url?(url)
+      URL_SUPPORTED_SCHEMES.include?(url.scheme) ? url : false
+    end
+
     # Check if installer was updated
     #
-    # It checks if a file UPDATED_FILENAME exists in Directory.vardir
+    # It checks if a file UPDATED_FLAG_FILENAME exists in Directory.vardir
     #
     # @return [Boolean] true if it exists; false otherwise.
     def installer_updated?
-      File.exist?(update_file)
+      File.exist?(update_flag_file)
     end
 
-    # Returns the name of the "update file"
+    # Returns the name of the "update flag file"
     #
-    # @return [String] Path to the "update file"
+    # @return [String] Path to the "update flag file"
     #
     # @see #update_installer
-    def update_file
-      File.join(Directory.vardir, UPDATED_FILENAME)
+    def update_flag_file
+      File.join(Directory.vardir, UPDATED_FLAG_FILENAME)
     end
   end
 end
--- test/inst_update_installer_test.rb
+++ test/inst_update_installer_test.rb
@@ -4,28 +4,71 @@ require_relative "test_helper"
 require "installation/clients/inst_update_installer"
 
 describe Yast::InstUpdateInstaller do
+  Yast.import "Linuxrc"
+  Yast.import "ProductFeatures"
+
   let(:manager) { double("update_manager") }
+  let(:url) { "http://update.opensuse.org/update.dud" }
 
   describe "#main" do
-    before do
-      allow(subject).to receive(:update_installer).and_return(update_result)
-    end
+    context "when update is enabled" do
+      before do
+        allow(subject).to receive(:self_update_enabled?).and_return(true)
+      end
 
-    context "when update works" do
-      let(:update_result) { true }
+      context "when update works" do
+        before do
+          allow(subject).to receive(:update_installer).and_return(true)
+        end
 
-      it "creates update file and returns :restart_yast" do
-        expect(::FileUtils).to receive(:touch)
-        expect(::FileUtils).to receive(:touch)
-        expect(subject.main).to eq(:restart_yast)
+        it "creates update file and returns :restart_yast" do
+          expect(::FileUtils).to receive(:touch).twice
+          allow(subject).to receive(:self_update_enabled?).and_return(true)
+          expect(subject.main).to eq(:restart_yast)
+        end
+      end
+
+      context "when update fails" do
+        before do
+          allow(subject).to receive(:update_installer).and_return(false)
+        end
+
+        it "does not create any file and returns :next" do
+          expect(::FileUtils).to_not receive(:touch)
+          expect(subject.main).to eq(:next)
+        end
+      end
+
+      context "when an URL is specified through Linuxrc" do
+        it "tries to update the installer using the given URL" do
+          allow(Yast::Linuxrc).to receive(:InstallInf).with("SelfUpdate").and_return(url)
+          allow(::Installation::UpdatesManager).to receive(:new).and_return(manager)
+          expect(manager).to receive(:add_update).with(URI(url))
+          expect(subject.main).to eq(:next)
+        end
+      end
+
+      context "when no URL is specified through Linuxrc" do
+
+        it "gets URL from control file" do
+          allow(Yast::ProductFeatures).to receive(:GetStringFeature).and_return(url)
+          allow(::Installation::UpdatesManager).to receive(:new).and_return(manager)
+          expect(manager).to receive(:add_update).with(URI(url))
+          expect(subject.main).to eq(:next)
+        end
+
+        context "and control file doesn't have an URL" do
+          it "does not update the installer" do
+            expect(subject).to_not receive(:update_installer)
+          end
+        end
       end
     end
 
-    context "when update fails" do
-      let(:update_result) { false }
-
-      it "does not create any file and returns :next" do
-        expect(::FileUtils).to_not receive(:touch)
+    context "when update is disabled through Linuxrc" do
+      it "does not update the installer" do
+        expect(Yast::Linuxrc).to receive(:InstallInf).with("SelfUpdate").and_return("0")
+        expect(subject).to_not receive(:update_installer)
         expect(subject.main).to eq(:next)
       end
     end
@@ -63,7 +106,6 @@ describe Yast::InstUpdateInstaller do
 
       it "returns true" do
         expect(subject.update_installer).to eq(false)
-
       end
     end
   end
