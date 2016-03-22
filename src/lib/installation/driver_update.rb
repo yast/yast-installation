f30f952c5ed5a8b011be275741a2e0ab0fbd6804
Imobach González Sosa <igonzalezsosa@suse.com>
2016-03-22 10:11:27 +0000

    Simplify DriverUpdate class
    * It does not retrieve remote updates anymore.
    * Add a new method DriverUpdate.find to search for local updates.
    * It will only responsible for applying the updates.

--- src/lib/installation/driver_update.rb
+++ src/lib/installation/driver_update.rb
@@ -14,9 +14,7 @@
 # ------------------------------------------------------------------------------
 
 require "yast"
-require "tempfile"
 require "pathname"
-require "transfer/file_from_url"
 
 module Installation
   # Represents a driver update disk (DUD)
@@ -24,99 +22,35 @@ module Installation
   # The DUD will be fetched from a given URL.
   class DriverUpdate
     include Yast::Logger
-    include Yast::I18n # missing in yast2-update
-    include Yast::Transfer::FileFromUrl # get_file_from_url
 
-    class NotFound < StandardError; end
     class CouldNotBeApplied < StandardError; end
     class PreScriptFailed < StandardError; end
 
-    # Command to extract the content of the DUD
-    EXTRACT_CMD = "gzip -dc %<source>s | cpio --quiet --sparse -dimu --no-absolute-filenames"
     # Command to apply the DUD disk to inst-sys
     APPLY_CMD = "/etc/adddir %<source>s/inst-sys /" # openSUSE/installation-images
-    # Command to extract content of a signed (with PGP) DUD
-    EXTRACT_SIG_CMD = "gpg --homedir %<homedir>s --batch --no-default-keyring --keyring %<keyring>s " \
-      "--ignore-valid-from --ignore-time-conflict --output '%<unpacked>s' '%<source>s'"
-    # Command to verify a detached PGP signature
-    VERIFY_SIG_CMD = "gpg --homedir %<homedir>s --batch --no-default-keyring --keyring %<keyring>s " \
-      "--ignore-valid-from --ignore-time-conflict --verify '%<signature>s' '%<data>s'"
-    # Temporary name for driver updates
-    TEMP_FILENAME = "remote.dud"
-    # Extension for unpacked driver updates after extracting the PGP signature
-    UNPACKED_EXT = ".unpacked"
-    # Extension for detached PGP signatures
-    SIG_EXT = ".asc"
-    # gpg output that means that signature is OK
-    GPG_SIGNATURE_OK = "gpg: Good signature"
-    GPG_SIGNED = "gpg: Signature made"
-    GPG_WARNING = "WARNING:"
 
-    attr_reader :uri, :local_path, :keyring, :gpg_homedir, :signature_status
+    attr_reader :path
+
+    class << self
+      # Find driver updates in a given directory
+      #
+      # A directory with a `dud.config` file will be considered a driver
+      # update.
+      #
+      # @param dir [Pathname] Directory to search for driver updates
+      # @return [Array<DriverUpdate>] Found driver updates
+      def find(dir)
+        Dir.glob("#{dir}/*/dud.config").map do |path|
+          new(Pathname(path))
+        end
+      end
+    end
 
     # Constructor
     #
-    # @param uri         [URI]      Driver Update URI
-    # @param keyring     [Pathname] Path to keyring to check signatures against
-    # @param gpg_homedir [Pathname] Path to GPG home dir
-    def initialize(uri, keyring, gpg_homedir)
-      Yast.import "Linuxrc"
-      @uri = uri
-      @local_path = nil
-      @keyring = keyring
-      @gpg_homedir = gpg_homedir
-      @signature_status = nil
-    end
-
-    # Determines whether a driver update is signed or not
-    #
-    # Signature is checked while fetching and extracting the driver update. The reason
-    # is that we need to check the original files and we don't want to keep them
-    # after the update is extracted (to save some memory during installation).
-    #
-    # The driver update will be considered signed if the signature is OK. It
-    # will be false otherwise. For more details, check #gpg_output_to_status.
-    #
-    # @return [Boolean] True if it's correctly signed; false otherwise.
-    #
-    # @see #signature_status
-    def signed?
-      [:ok, :warning].include?(signature_status)
-    end
-
-    # Fetch the DUD and extract it in the given directory
-    #
-    # @param target [Pathname] Directory to extract the DUD to.
-    def fetch(target)
-      @local_path = target
-      Dir.mktmpdir do |dir|
-        temp_file = Pathname(dir).join(TEMP_FILENAME)
-        download_file_to(temp_file)
-        clear_attached_signature(temp_file)
-        check_detached_signature(temp_file) unless signed?
-        extract(temp_file, local_path)
-      end
-    end
-
-    # Determine if gpg command was successful
-    #
-    # * :ok:      Signature is ok.
-    # * :warning: Signature is ok but with some warning (for example not trusted ones).
-    # * :error:   Signature is invalid (maybe public key is missing).
-    # * :missing: Signature is missing.
-    #
-    # @return [Symbol] Signature status
-    #
-    # @see GPG_SIGNATURE_OK
-    def gpg_output_to_status(out)
-      log.info("Checking gpg output: #{out}")
-      if out["stderr"].include?(GPG_SIGNATURE_OK)
-        out["stderr"].include?(GPG_WARNING) ? :warning : :ok
-      elsif out["stderr"].include?(GPG_SIGNED)
-        :error
-      else
-        :missing
-      end
+    # @param path [Pathname] Path to driver update
+    def initialize(path)
+      @path = path
     end
 
     # Apply the DUD to inst-sys
@@ -124,75 +58,17 @@ module Installation
     # @see #adddir
     # @see #run_update_pre
     def apply
-      raise "Driver updated not fetched yet!" if local_path.nil?
       adddir
       run_update_pre
-    end
-
-  private
-
-    # Extract the DUD at 'source' to 'target'
-    #
-    # @param source [Pathname]
-    #
-    # @see EXTRACT_CMD
-    def extract(source, target)
-      cmd = format(EXTRACT_CMD, source: source)
-      Dir.chdir(source.dirname) do
-        out = Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), cmd)
-        raise "Could not extract DUD" unless out["exit"].zero?
-        setup_target(target)
-        ::FileUtils.mv(update_dir.realpath, target)
-      end
-    end
-
-    # Clear and check an attached signature
-    #
-    # If the file at 'path' is signed, it will extract its content checking its
-    # signature. As a side effect, the extracted file will be placed in 'path'.
-    #
-    # @return [Boolean] True if the package was successfuly signed; false otherwise.
-    def clear_attached_signature(path)
-      unpacked_path = path.sub_ext(UNPACKED_EXT)
-      cmd = format(EXTRACT_SIG_CMD, source: path, unpacked: unpacked_path,
-                   keyring: keyring, homedir: gpg_homedir)
-      out = Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), cmd)
-      ::FileUtils.mv(unpacked_path, path) if unpacked_path.exist?
-      @signature_status = gpg_output_to_status(out)
-    end
-
-    # Set up the target directory
-    #
-    # Refresh the target directory (dir will be re-created).
-    #
-    # @param dir [Pathname] Directory to re-create
-    def setup_target(dir)
-      ::FileUtils.rm_r(dir) if dir.exist?
-      ::FileUtils.mkdir_p(dir.dirname) unless dir.dirname.exist?
-    end
-
-    # Download the DUD to a file
-    #
-    # If the file is not downloaded, DriverUpdate::NotFound exception is risen.
-    #
-    # @raise DriverUpdate::NotFound
-    def download_file_to(path)
-      raise NotFound unless get_file(uri, path)
-    end
-
-    # Directory which contains files within the DUD
-    #
-    # @see UpdateDir value at /etc/install.inf.
-    def update_dir
-      path = Pathname.new(Yast::Linuxrc.InstallInf("UpdateDir"))
-      path.relative_path_from(Pathname.new("/"))
     end
 
     # Add files/directories to the inst-sys
     #
     # @see APPLY_CMD
+    #
+    # @raise CouldNotBeApplied
     def adddir
-      cmd = format(APPLY_CMD, source: local_path)
+      cmd = format(APPLY_CMD, source: path)
       out = Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), cmd)
       raise CouldNotBeApplied unless out["exit"].zero?
     end
@@ -201,44 +77,15 @@ module Installation
     #
     # @return [Boolean] true if execution was successful; false if
     #                   update script didn't exist.
-    # @raise DriverUpdate::CouldNotBeApplied
+    #
+    # @raise DriverUpdate::PreScriptFailed
     def run_update_pre
-      update_pre_path = local_path.join("install", "update.pre")
+      update_pre_path = path.join("install", "update.pre")
       return false unless update_pre_path.exist? && update_pre_path.executable?
       out = Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), update_pre_path.to_s)
       log.info("update.pre script at #{update_pre_path} was executed: #{out}")
       raise PreScriptFailed unless out["exit"].zero?
       true
-    end
-
-    # Wrapper to get a file using Yast::Transfer::FileFromUrl#get_file_from_url
-    #
-    # @return [Boolean] true if the file was retrieved; false otherwise.
-    def get_file(location, path)
-      get_file_from_url(scheme: location.scheme, host: location.host, urlpath: location.path,
-                        localfile: path.to_s, urltok: {}, destdir: "")
-    end
-
-    # Check a detached signature for a DUD
-    #
-    # The signature will be taken from the same URL than the DUD but adding
-    # the suffix '.asc'.
-    #
-    # @return [Boolean] True if the signature is OK; false otherwise.
-    #
-    # @see #gpg_output_to_status
-    def check_detached_signature(temp_file)
-      # Download the detached signature
-      asc_file = temp_file.sub_ext("#{temp_file.extname}#{SIG_EXT}")
-      log.info("Downloading #{asc_file} to check the signature")
-      return false unless get_file(uri.merge("#{uri}#{SIG_EXT}"), asc_file)
-
-      # Verify the signature
-      cmd = format(VERIFY_SIG_CMD, signature: asc_file, data: temp_file,
-                   keyring: keyring, homedir: gpg_homedir)
-      out = Yast::SCR.Execute(Yast::Path.new(".target.bash_output"), cmd)
-      ::FileUtils.rm(asc_file) if asc_file.exist?
-      @signature_status = gpg_output_to_status(out) # Set signature status
     end
   end
 end
--- test/driver_update_test.rb
+++ test/driver_update_test.rb
@@ -8,184 +8,55 @@ require "uri"
 Yast.import "Linuxrc"
 
 describe Installation::DriverUpdate do
-  TEST_DIR = Pathname.new(__FILE__).dirname
-  TEMP_DIR = TEST_DIR.join("test", "tmp", "update")
-  FIXTURES_DIR = TEST_DIR.join("fixtures")
+  FIXTURES_DIR = Pathname.new(__FILE__).dirname.join("fixtures")
 
-  let(:url) { URI("file://#{FIXTURES_DIR}/fake.signed.dud") }
-  let(:keyring) { FIXTURES_DIR.join("pubring.gpg") }
-  let(:gpg_homedir) { FIXTURES_DIR.join("dot.gnupg") }
-  let(:target) { TEST_DIR.join("target") }
+  subject(:dud) { Installation::DriverUpdate.new(update_path) }
+  let(:update_path) { FIXTURES_DIR.join("updates", "000") }
 
-  subject { Installation::DriverUpdate.new(url, keyring, gpg_homedir) }
-
-  before do
-    allow(Yast::Linuxrc).to receive(:InstallInf).with("UpdateDir")
-      .and_return("/linux/suse/x86_64-sles12")
-    ::FileUtils.chmod(0700, gpg_homedir)
-  end
-
-  after do
-    # Make sure those files are removed
-    ::FileUtils.rm_r(TEMP_DIR) if TEMP_DIR.exist?
-    ::FileUtils.rm_r(target) if target.exist?
-  end
-
-  describe "#fetch" do
-    let(:url) { URI("file://#{FIXTURES_DIR}/fake.signed.dud") }
-
-    it "downloads the file at #url and stores in the given directory" do
-      subject.fetch(target)
-      expect(target.join("dud.config")).to be_file
+  describe ".find" do
+    context "when no updates exist" do
+      it "returns an empty array" do
+        expect(described_class.find(FIXTURES_DIR)).to eq([])
+      end
     end
 
-    context "when the remote file does not exists" do
-      let(:url) { URI("http://non-existent-url.com/") }
-
-      it "raises an exception" do
-        expect { subject.fetch(target) }.to raise_error StandardError
+    context "when updates exist" do
+      it "returns an array of driver updates" do
+        updates = described_class.find(FIXTURES_DIR.join("updates"))
+        expect(updates).to all(be_an(described_class))
       end
     end
   end
 
-  describe "#signature" do
-    context "if the signature is attached" do
-      context "and signature is valid and trusted" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.signed.dud") }
-
-        it "returns :ok" do
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:ok)
-        end
-      end
-
-      context "and signature is valid but not trusted" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.signed+untrusted.dud") }
-
-        it "returns :warning" do
-          allow(subject).to receive(:get_file).with(any_args).and_call_original
-          allow(subject).to receive(:get_file)
-            .with(URI("file://#{FIXTURES_DIR}/fake.signed+untrusted.dud.asc"), any_args).and_return(false)
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:warning)
-        end
-      end
-
-      context "and signature is :error" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.signed+unknown.dud") }
-
-        it "returns false" do
-          allow(subject).to receive(:get_file).with(any_args).and_call_original
-          allow(subject).to receive(:get_file)
-            .with(URI("file://#{FIXTURES_DIR}/fake.signed+unknown.dud.asc"), any_args).once.and_return(false)
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:error)
-        end
-      end
-    end
-
-    context "signature is detached" do
-      context "and signature is valid and trusted" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.detached.dud") }
-
-        it "returns true" do
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:ok)
-        end
-      end
-
-      context "and signature is valid but not trusted" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.detached+untrusted.dud") }
-
-        it "returns true" do
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:warning)
-        end
-      end
-
-      context "and signature is unknown" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.detached+unknown.dud") }
-
-        it "returns :error" do
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:error)
-        end
-      end
-
-      context "and .asc file does not exist" do
-        let(:url) { URI("file://#{FIXTURES_DIR}/fake.dud") }
-
-        before do
-          allow(subject).to receive(:get_file).with(any_args).and_call_original
-          allow(subject).to receive(:get_file)
-            .with(URI("file://#{FIXTURES_DIR}/fake.dud.asc"), any_args).once.and_return(false)
-        end
-
-        it "returns :missing" do
-          subject.fetch(target)
-          expect(subject.signature_status).to eq(:missing)
-        end
-      end
-    end
-  end
-
-  describe "#signed?" do
-    before { expect(subject).to receive(:signature_status).and_return(status) }
-
-    context "present and good" do
-      let(:status) { :ok }
-
-      it "returns true" do
-        expect(subject).to be_signed
-      end
-    end
-
-    context "good but with a warning" do
-      let(:status) { :warning }
-
-      it "returns true" do
-        expect(subject).to be_signed
-      end
-    end
-
-    context "signed with and unknown key" do
-      let(:status) { :error }
-
-      it "returns false" do
-        expect(subject).to_not be_signed
-      end
-    end
-
-    context "is signature is missing" do
-      let(:status) { :missing }
-
-      it "returns false" do
-        expect(subject).to_not be_signed
-      end
+  describe "#path" do
+    it "returns the path where the DUD is located" do
+      expect(dud.path).to eq(update_path)
     end
   end
 
   describe "#apply" do
-    let(:local_path) { TEMP_DIR.join("000") }
-
     context "when the remote file was fetched" do
-      before do
-        subject.fetch(local_path)
-      end
-
-      it "applies the DUD to the running system" do
+      it "applies the DUD and executes update.pre script" do
         expect(Yast::SCR).to receive(:Execute)
-          .with(Yast::Path.new(".target.bash_output"), "/etc/adddir #{local_path}/inst-sys /")
-          .and_return("exit" => 0)
-        subject.apply
+          .with(Yast::Path.new(".target.bash_output"), "/etc/adddir #{update_path}/inst-sys /")
+          .and_return("exit" => 0, "stdout" => "", "stderr" => "")
+        expect(Yast::SCR).to receive(:Execute)
+          .with(Yast::Path.new(".target.bash_output"), "#{update_path}/install/update.pre")
+          .and_return("exit" => 0, "stdout" => "", "stderr" => "")
+        dud.apply
       end
-    end
 
-    context "when the remote file was not fetched" do
-      let(:local_path) { nil }
+      context "if an update.pre does not exist" do
+        let(:update_path) { FIXTURES_DIR.join("updates", "001") }
 
-      it "raises an exception" do
-        expect { subject.apply }.to raise_error(RuntimeError)
+        it "does not try to run the update.pre script" do
+          expect(Yast::SCR).to receive(:Execute)
+            .with(Yast::Path.new(".target.bash_output"), /adddir/)
+            .and_return("exit" => 0)
+          expect(Yast::SCR).to_not receive(:Execute)
+            .with(Yast::Path.new(".target.bash_output"), "update.pre")
+          dud.apply
+        end
       end
     end
   end
