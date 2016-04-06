a480c60963f256216c283fb14ac7971384f92575
Knut Alejandro Anderssen González <teclator@gmail.com>
2016-04-06 09:12:40 +0100

    Fix some mocks.

--- test/inst_disks_activate_test.rb
+++ test/inst_disks_activate_test.rb
@@ -10,21 +10,25 @@ describe Yast::InstDisksActivateClient do
   Yast.import "GetInstArgs"
   Yast.import "UI"
   Yast.import "Popup"
+  Yast.import "Storage"
 
   describe "#main" do
     let(:probed_disks) { [] }
+    let(:s390) { false }
 
     before do
       allow(Yast::Linuxrc).to receive(:InstallInf).with("WithFCoE").and_return("0")
       allow(Yast::UI).to receive(:OpenDialog)
       allow(Yast::UI).to receive(:CloseDialog)
       allow(Yast::Popup).to receive(:ConfirmAbort).with(:painless).and_return(true)
+      allow(Yast::Arch).to receive(:s390).and_return(s390)
+      allow(Yast::Storage).to receive(:ReReadTargetMap)
     end
 
     context "when architecture is s390" do
+      let(:s390) { true }
       before do
         allow(Yast::UI).to receive(:UserInput).and_return(:abort)
-        allow(Yast::Arch).to receive(:s390).and_return(true)
       end
 
       it "detects DASD disks" do
@@ -46,45 +50,44 @@ describe Yast::InstDisksActivateClient do
 
         expect(subject.main).to eq(:abort)
       end
+    end
 
-      context "when dasd button is clicked" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:dasd, :abort)
-        end
-
-        it "calls inst_dasd client" do
-          expect(Yast::WFM).to receive(:call).with("inst_dasd")
-          expect(subject).to receive(:show_base_dialog).twice
-
-          subject.main
-        end
+    context "when dasd button is clicked" do
+      before do
+        allow(Yast::UI).to receive(:UserInput).and_return(:dasd, :abort)
       end
 
-      context "when network button is clicked" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:network, :abort)
-        end
+      it "calls inst_dasd client" do
+        expect(Yast::WFM).to receive(:call).with("inst_dasd")
+        expect(subject).to receive(:show_base_dialog).twice
 
-        it "calls inst_lan client" do
-          expect(Yast::WFM).to receive(:call).with("inst_lan", ["skip_detection" => true])
-          expect(subject).to receive(:show_base_dialog).twice
+        subject.main
+      end
+    end
 
-          subject.main
-        end
+    context "when network button is clicked" do
+      before do
+        allow(Yast::UI).to receive(:UserInput).and_return(:network, :abort)
       end
 
-      context "when abort button is clicked" do
-        before do
-          allow(Yast::UI).to receive(:UserInput).and_return(:abort)
-        end
+      it "calls inst_lan client" do
+        expect(Yast::WFM).to receive(:call).with("inst_lan", ["skip_detection" => true])
+        expect(subject).to receive(:show_base_dialog).twice
 
-        it "returns :abort" do
-          expect(subject).to receive(:show_base_dialog).once
+        subject.main
+      end
+    end
 
-          expect(subject.main).to eq(:abort)
-        end
+    context "when abort button is clicked" do
+      before do
+        allow(Yast::UI).to receive(:UserInput).and_return(:abort)
       end
 
+      it "returns :abort" do
+        expect(subject).to receive(:show_base_dialog).once
+
+        expect(subject.main).to eq(:abort)
+      end
     end
   end
 end
