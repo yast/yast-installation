28098cef5167b545135a2b8c083a6db0c1ec4d10
Stefan Schubert <schubi@suse.de>
2016-05-12 11:40:13 +0200

    taking %s

--- src/lib/installation/clients/inst_complex_welcome.rb
+++ src/lib/installation/clients/inst_complex_welcome.rb
@@ -437,7 +437,7 @@ module Yast
                       Label(
                         # TRANSLATORS: addition license information
                         # %1 is replaced with the filename
-                        _("EULA location in the installed system:\n%1") %
+                        _("EULA location in the installed system:\n%s") %
                           license_location
                       )
                     )
