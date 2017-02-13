d40c349b3610f69e0fde9f9eaef911da9a22e2f4
Knut Alejandro Anderssen González <teclator@gmail.com>
2017-02-13 13:00:26 +0000

    Use single quotes in case of dashed url.

--- src/lib/installation/system_roles/handlers.rb
+++ src/lib/installation/system_roles/handlers.rb
@@ -34,7 +34,10 @@ module Installation
           log.info("The minion master.conf file does not exist, it will be created")
         end
         log.info("The controller node for this worker role is: #{master}")
-        master_conf.master = master
+        # FIXME: the cobblersettings lense does not support dashes in the url
+        # without single quotes, we need to use a custom lense for salt conf.
+        # As Salt can use also 'url' just use in case of dashed.
+        master_conf.master = master.include?("-") ? "'#{master}'" : master
         master_conf.save
       end
     end
