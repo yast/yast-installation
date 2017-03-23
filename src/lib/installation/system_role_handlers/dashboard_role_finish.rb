e881523899900f4793095826190168d69986ddbd
Imobach González Sosa <igonzalezsosa@suse.com>
2017-03-23 09:51:14 +0000

    Use Yast::Execute instead of Yast::SCR in Dashboard handler

--- src/lib/installation/system_role_handlers/dashboard_role_finish.rb
+++ src/lib/installation/system_role_handlers/dashboard_role_finish.rb
@@ -1,27 +1,15 @@
 require "yast"
-Yast.import "Popup"
-Yast.import "FileUtils"
+require "yast2/execute"
 
 module Installation
   module SystemRoleHandlers
     class DashboardRoleFinish
-      include Yast::I18n
-      include Yast::Logger
-
       # Path to the activation script
       ACTIVATION_SCRIPT_PATH = "/usr/share/caasp-container-manifests/activate.sh".freeze
 
       # Run the activation script
       def run
-        if !Yast::FileUtils.Exists(ACTIVATION_SCRIPT_PATH)
-          Yast::Popup.Error(_("Activation script not found:\n#{ACTIVATION_SCRIPT_PATH}"))
-          return
-        end
-
-        out = Yast::SCR.Execute(Yast::Term.new(".target.bash_output"), ACTIVATION_SCRIPT_PATH)
-        return if out["exit"].zero?
-
-        Yast::Popup.LongError(out["stdout"])
+        Yast::Execute.on_target(ACTIVATION_SCRIPT_PATH)
       end
     end
   end
--- test/lib/system_role_handlers/dashboard_role_finish_test.rb
+++ test/lib/system_role_handlers/dashboard_role_finish_test.rb
@@ -1,45 +1,15 @@
 #! /usr/bin/env rspec
 
 require_relative "../../test_helper"
-require "installation/system_role"
 require "installation/system_role_handlers/dashboard_role_finish"
 
 describe Installation::SystemRoleHandlers::DashboardRoleFinish do
   subject(:handler) { Installation::SystemRoleHandlers::DashboardRoleFinish.new }
 
   describe "#run" do
-    let(:script_exists) { true }
-
-    before do
-      allow(Yast::FileUtils).to receive(:Exists).with(/activate.sh/)
-        .and_return(script_exists)
-    end
-
     it "runs the activation script" do
-      expect(Yast::SCR).to receive(:Execute)
-        .with(Yast::Term.new(".target.bash_output"), /activate.sh/)
-        .and_return("exit" => 0)
+      expect(Yast::Execute).to receive(:on_target).with(/activate.sh/)
       handler.run
-    end
-
-    context "when the script is not found" do
-      let(:script_exists) { false }
-
-      it "informs the user" do
-        expect(Yast::Popup).to receive(:Error)
-        handler.run
-      end
-    end
-
-    context "when the script fails" do
-      it "shows the error to the user" do
-        allow(Yast::SCR).to receive(:Execute)
-          .with(Yast::Term.new(".target.bash_output"), /activate.sh/)
-          .and_return("exit" => 1, "stderr" => "Some error")
-        expect(Yast::Popup).to receive(:LongError)
-          .with(/Some error/)
-        handler.run
-      end
     end
   end
 end
