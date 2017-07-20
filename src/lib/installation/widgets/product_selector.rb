9a72f018c4625091937383ce6f7659b2c3b3f379
Ladislav Slezák <lslezak@suse.cz>
2017-07-20 16:15:29 +0200

    Select the product to install

--- src/lib/installation/widgets/product_selector.rb
+++ src/lib/installation/widgets/product_selector.rb
@@ -5,10 +5,11 @@ require "cwm/common_widgets"
 module Installation
   module Widgets
     class ProductSelector < CWM::RadioButtons
-      attr_reader :items
+      attr_reader :items, :products
 
       # @param products [Array<Installation::Product>] to display
       def initialize(products)
+        @products = products
         @items = products.map { |p| [p.name, p.label, p.selected?] }
         textdomain "installation"
       end
@@ -19,6 +20,12 @@ module Installation
 
       def label
         _("Product to Install")
+      end
+
+      def store
+        # TODO: deselect the previously selected product when going back
+        product = products.find { |p| p.name == value }
+        product.select if product
       end
     end
   end
