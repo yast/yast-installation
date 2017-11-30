204fcbd8372a3b5c66c078868a055b6bfdde692e
Imobach González Sosa <igonzalezsosa@suse.com>
2017-11-30 14:34:11 +0000

    Use AutoYaST activate callbacks when needed

---
+++ test/lib/clients/inst_system_analysis_test.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env rspec
+# Copyright (c) [2017] SUSE LLC
+#
+# All Rights Reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 2 of the GNU General Public License as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, contact SUSE LLC.
+#
+# To contact SUSE LLC about this file by physical or electronic mail, you may
+# find current contact information at www.suse.com.
+
+require_relative "../../test_helper"
+require "installation/clients/inst_system_analysis"
+
+describe Yast::InstSystemAnalysisClient do
+  subject(:client) { described_class.new }
+
+  describe "#ActionHDDProbe" do
+    let(:storage) do
+      instance_double(Y2Storage::StorageManager, activate: true, probe: nil, probed: devicegraph)
+    end
+
+    let(:devicegraph) { instance_double(Y2Storage::Devicegraph, empty?: false) }
+    let(:autoyast_callbacks) { double("autoyast_callbacks") }
+    let(:auto) { false }
+
+    before do
+      allow(Y2Storage::StorageManager).to receive(:instance).and_return(storage)
+      allow(Yast::Mode).to receive(:auto).and_return(auto)
+      stub_const("Y2Autoinstallation::ActivateCallbacks", autoyast_callbacks)
+    end
+
+    it "uses default activation callbacks" do
+      expect(storage).to receive(:activate).with(nil)
+      client.ActionHDDProbe
+    end
+
+    context "when running AutoYaST" do
+      let(:auto) { true }
+
+      it "uses AutoYaST activation callbacks" do
+        expect(storage).to receive(:activate).with(autoyast_callbacks)
+        client.ActionHDDProbe
+      end
+    end
+  end
+end
