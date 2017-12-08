46672b628611ad976bb5a2bca2c56e3e9031d5e2
Gilson Souza <gsouzadossantos@suse.com>
2017-12-08 15:16:50 +0100

    fate#324198: filter modules extensions based on product

--- src/lib/installation/clients/inst_modules_extensions.rb
+++ src/lib/installation/clients/inst_modules_extensions.rb
@@ -1,4 +1,5 @@
 require "yast"
+require "y2packager/product"
 
 Yast.import "GetInstArgs"
 Yast.import "Pkg"
@@ -21,10 +22,28 @@ module Installation
     private
 
       PROVIDES_KEY = "installer_module_extension()".freeze
+      PROVIDES_PRODUCT = "extension_for_product()".freeze
 
       def extension_packages
+        product = Y2Packager::Product.selected_base
         extension_packages = Yast::Pkg.PkgQueryProvides(PROVIDES_KEY)
         log.info "module extension packages #{extension_packages.inspect}"
+
+        extension_packages.select! do |list|
+          pkg_name = list.first
+          dependencies = Yast::Pkg.ResolvableDependencies(pkg_name, :package, "").first["deps"]
+
+          product_provides = dependencies.find_all do |d|
+            d["provides"] && d["provides"].match(/#{Regexp.escape(PROVIDES_PRODUCT)}/)
+          end
+
+          target_product = product_provides.any? do |d|
+            d["provides"][/#{Regexp.escape(PROVIDES_PRODUCT)}\s*=\s*(\S+)/, 1] == product.name
+          end
+
+          # If no product is specified for the role, it should be available to all products
+          product_provides.empty? || target_product
+        end
 
         extension_packages.map do |list|
           pkg_name = list.first
