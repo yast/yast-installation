39ce165f000b9135f9355cedf7d907a944dcaee7
Stefan Schubert <schubi@suse.de>
2017-12-20 14:03:27 +0100

    rubocop

--- src/lib/installation/proposal_runner.rb
+++ src/lib/installation/proposal_runner.rb
@@ -86,10 +86,10 @@ module Installation
           # Fit to the given UI
           displayinfo = Yast::UI.GetDisplayInfo || {}
           width = displayinfo["TextMode"] ? displayinfo.fetch("Width", 80) : 80
-          Yast::Report.Warning(
-            wrap_text(error_message, width - 4)) unless error_message.empty?
-          Yast::Report.Warning(
-            wrap_text(second_stage_error, width - 4)) unless second_stage_error.empty?
+          Yast::Report.Warning( wrap_text(error_message,
+            width - 4)) unless error_message.empty?
+          Yast::Report.Warning( wrap_text(second_stage_error,
+            width - 4)) unless second_stage_error.empty?
           # skip if not interactive mode.
           return :auto
         else
