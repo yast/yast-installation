3a8f0020b9f2302b93c65ffb5efbc8f87d080a0d
Ladislav Slezák <lslezak@suse.cz>
2018-05-03 12:56:59 +0200

    Better handle nil value

--- src/lib/installation/clients/inst_complex_welcome.rb
+++ src/lib/installation/clients/inst_complex_welcome.rb
@@ -194,7 +194,8 @@ module Yast
     # @return [Boolean] true if a product has been selected and license
     # agreement confirmed when required; false otherwise
     def product_selection_finished?
-      if selected_product.nil? && products.size > 1
+      if selected_product.nil?
+        return true if products.size <= 1
         Yast::Popup.Error(_("Please select a product to install."))
         return false
       elsif license_confirmation_required? && !selected_product.license_confirmed?
