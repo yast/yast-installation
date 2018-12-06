801dce982bcf36325917270099e7b5c405eab9b0
Ladislav Slezák <lslezak@suse.cz>
2018-12-06 14:20:30 +0100

    Adapt the testsuite

--- test/lib/clients/save_y2logs_test.rb
+++ test/lib/clients/save_y2logs_test.rb
@@ -11,23 +11,40 @@ describe Yast::SaveY2logs do
   describe "#main" do
     let(:bash_path) { Yast::Path.new(".local.bash") }
 
+    before do
+      allow(Yast::Installation).to receive(:destdir).and_return("/mnt")
+      allow(File).to receive(:exist?).and_return(false)
+    end
+
     context "globals/save_y2logs in control.xml is false" do
       it "does not save y2logs" do
         expect(Yast::ProductFeatures).to receive(:GetBooleanFeature).with("globals", "save_y2logs").and_return(false)
         expect(Yast::WFM).not_to receive(:Execute).with(
           bash_path,
-          "/usr/sbin/save_y2logs '/var/log/YaST2/yast-installation-logs.tar.xz'"
+          /save_y2logs/
         )
         client.main
       end
     end
 
     context "globals/save_y2logs in control.xml is true" do
-      it "saves y2logs" do
+      before do
         expect(Yast::ProductFeatures).to receive(:GetBooleanFeature).with("globals", "save_y2logs").and_return(true)
+      end
+
+      it "saves y2logs" do
         expect(Yast::WFM).to receive(:Execute).with(
           bash_path,
-          "/usr/sbin/save_y2logs '/var/log/YaST2/yast-installation-logs.tar.xz'"
+          "TMPDIR=/tmp /usr/sbin/save_y2logs '/mnt/var/log/YaST2/yast-installation-logs.tar.xz'"
+        )
+        client.main
+      end
+
+      it "uses the target /tmp when it exists" do
+        expect(File).to receive(:exist?).with("/mnt/tmp").and_return(true)
+        expect(Yast::WFM).to receive(:Execute).with(
+          bash_path,
+          "TMPDIR=/mnt/tmp /usr/sbin/save_y2logs '/mnt/var/log/YaST2/yast-installation-logs.tar.xz'"
         )
         client.main
       end
