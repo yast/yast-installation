56c4e2070af1cf827aca89ab11e5a50641b7c70a
Stefan Schubert <schubi@suse.de>
2019-11-22 11:04:27 +0100

    rubocop

--- test/lib/clients/inst_extrasources_test.rb
+++ test/lib/clients/inst_extrasources_test.rb
@@ -9,7 +9,7 @@ describe Yast::InstExtrasourcesClient do
       source = 42
       expect(Yast::Pkg).to receive(:GetPackages).and_return(["foo"])
       expect(Y2Packager::Resolvable).to receive(:find)
-        .with(kind: :package, name: "foo", :status=>:selected)
+        .with(kind: :package, name: "foo", status: :selected)
         .and_return([Y2Packager::Resolvable.new(
           "kind" => :package, "name" => "foo", "version" => "1.0", "arch" => "x86_64",
           "status" => :selected, "source" => source
