35ccc087c43d71e9ff1e93c32d6aad1b62f202a9
Steffen Winterfeldt <wfeldt@opensuse.org>
2020-11-26 14:45:02 +0100

    update unit tests

--- test/dialogs/complex_welcome_test.rb
+++ test/dialogs/complex_welcome_test.rb
@@ -4,6 +4,22 @@ require_relative "../test_helper"
 require "installation/dialogs/complex_welcome"
 
 describe Installation::Dialogs::ComplexWelcome do
+  RSpec.shared_examples "show_license" do
+    it "shows the product license" do
+      expect(Y2Packager::Widgets::ProductLicense).to receive(:new)
+        .with(products.first, skip_validation: true).and_return(license_widget)
+      expect(widget.contents.to_s).to include("license_widget")
+    end
+  end
+
+  RSpec.shared_examples "show_selector" do
+    it "shows the product selector" do
+      expect(Installation::Widgets::ProductSelector).to receive(:new)
+        .with(products, skip_validation: true)
+      expect(widget.contents.to_s).to include("selector_widget")
+    end
+  end
+
   subject(:widget) { described_class.new(products) }
 
   let(:products) { [] }
@@ -27,7 +43,14 @@ describe Installation::Dialogs::ComplexWelcome do
   end
 
   describe "#content" do
-    let(:sles_product) { instance_double("Y2Packager::Product", label: "SLES") }
+    let(:license) { instance_double("Y2Packager::ProductLicense") }
+    # there are 3 different 'product' classes: Y2Packager::Product, Y2Packager::ProductControlProduct, Y2Packager::ProductLocation
+    let(:sles_product) { instance_double("Y2Packager::Product", label: "SLES", license: license) }
+    let(:sles_online_product) { instance_double("Y2Packager::ProductControlProduct", label: "SLES", license: license) }
+    let(:sles_offline_product) { instance_double("Y2Packager::ProductLocation", label: "SLES") }
+    let(:sled_product) { instance_double("Y2Packager::Product", label: "SLED", license: license) }
+    let(:sled_online_product) { instance_double("Y2Packager::ProductControlProduct", label: "SLED", license: license) }
+    let(:sled_offline_product) { instance_double("Y2Packager::ProductLocation", label: "SLED") }
     let(:language_widget) { Yast::Term.new(:language_widget) }
     let(:keyboard_widget) { Yast::Term.new(:keyboard_widget) }
     let(:license_widget) { Yast::Term.new(:license_widget) }
@@ -50,23 +73,32 @@ describe Installation::Dialogs::ComplexWelcome do
     end
 
     context "when only 1 product is available" do
-      let(:products) { [sles_product] }
-
-      it "shows the product license" do
-        expect(Y2Packager::Widgets::ProductLicense).to receive(:new)
-          .with(products.first, skip_validation: true).and_return(license_widget)
-        expect(widget.contents.to_s).to include("license_widget")
+      context "when it is the normal medium" do
+        let(:products) { [sles_product] }
+        include_examples "show_license"
+      end
+      context "when it is the online medium" do
+        let(:products) { [sles_online_product] }
+        include_examples "show_license"
+      end
+      context "when it is the offline medium" do
+        let(:products) { [sles_offline_product] }
+        include_examples "show_selector"
       end
     end
 
     context "when more than 1 product is available" do
-      let(:sled_product) { instance_double("Y2Packager::Product", label: "SLED") }
-      let(:products) { [sles_product, sled_product] }
-
-      it "shows the product selector" do
-        expect(Installation::Widgets::ProductSelector).to receive(:new)
-          .with(products, skip_validation: true)
-        expect(widget.contents.to_s).to include("selector_widget")
+      context "when it is the normal medium" do
+        let(:products) { [sles_product, sled_product] }
+        include_examples "show_selector"
+      end
+      context "when it is the online medium" do
+        let(:products) { [sles_online_product, sled_online_product] }
+        include_examples "show_selector"
+      end
+      context "when it is the offline medium" do
+        let(:products) { [sles_offline_product, sled_offline_product] }
+        include_examples "show_selector"
       end
     end
   end
