e793776a0694f5b98285aff74fe6d011c4c8fab0
Josef Reidinger <jreidinger@suse.cz>
2021-01-28 19:41:50 +0100

    fix firewall namespace

--- src/lib/installation/dialogs/security.rb
+++ src/lib/installation/dialogs/security.rb
@@ -49,7 +49,7 @@ module Installation
                 0.5,
                 0.5,
                 VBox(
-                  Widgets::FirewallSSHProposal.new(@settings)
+                  Y2Firewall::Widgets::FirewallSSHProposal.new(@settings)
                 )
               )
             )
