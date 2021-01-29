370d562b751bcb00a1f02050fdff05a60a95041b
Josef Reidinger <jreidinger@suse.cz>
2021-01-29 12:26:40 +0100

    add widget for selinux policy

---
+++ src/lib/installation/widgets/selinux_policy.rb
@@ -0,0 +1,47 @@
+# ------------------------------------------------------------------------------
+# Copyright (c) 2021 SUSE LLC
+#
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of version 2 of the GNU General Public License as published by the
+# Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, contact SUSE.
+#
+# To contact SUSE about this file by physical or electronic mail, you may find
+# current contact information at www.suse.com.
+# ------------------------------------------------------------------------------
+
+require "yast"
+require "cwm/common_widgets"
+
+module Installation
+  module Widgets
+    class SelinuxPolicy < CWM::ComboBox
+      def initialize(settings)
+        textdomain "installation"
+
+        @settings = settings
+      end
+
+      def label
+        _("SELinux Policy")
+      end
+
+      def items
+        # TODO:
+      end
+
+      def init
+        # TODO:
+      end
+
+      def store
+        # TODO:
+      end
+    end
