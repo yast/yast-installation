018fd86eeb674b8d0bc13eaa416ee5f6b50fab78
Ladislav Slezák <lslezak@suse.cz>
2021-03-05 14:04:26 +0100

    Rubocop fix

--- src/lib/installation/console/commands.rb
+++ src/lib/installation/console/commands.rb
@@ -48,7 +48,7 @@ module Installation
         # %{cmd} is replaced by a command name
         # %{keys} is replaced by a keyboard shortcut
         puts _("Type '%{cmd}' or press %{keys} to close the console and go back " \
-          "to the installer") % {cmd: "quit", keys: "Ctrl+D"}
+          "to the installer") % { cmd: "quit", keys: "Ctrl+D" }
         puts
         puts "Type 'commands' to see the available special commands"
         puts
