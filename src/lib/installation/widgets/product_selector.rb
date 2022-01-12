d7de9d56ec9e2f07077b994ba873e094da65cd02
Josef Reidinger <jreidinger@suse.cz>
2022-01-12 18:15:39 +0100

    one more recent year

--- src/lib/installation/widgets/product_selector.rb
+++ src/lib/installation/widgets/product_selector.rb
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------------
-# Copyright (c) 2021 SUSE LLC
+# Copyright (c) 2022 SUSE LLC
 #
 #
 # This program is free software; you can redistribute it and/or modify it under
